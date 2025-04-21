5bc3baefa4cbf4d688dc67b707a251f678c507e7
mag123c <diehreo@gmail.com>
2025-04-22 00:21:58 +0900

    fix: batch semaphore

--- src/parser/asynchronous/review_parser.py
+++ src/parser/asynchronous/review_parser.py
@@ -21,8 +21,20 @@ class DanawaReviewAsyncParser:
                 f"{self.BASE_REVIEW_URL}?prodCode={product_code}"
                 f"&page={page_num}&limit={limit}&score=0&t={t}"
             )
+            cookies = await self.page.context.cookies()
+            cookie_header = "; ".join([f"{c['name']}={c['value']}" for c in cookies])
+            
+            headers = {
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Referer": f"https://prod.danawa.com/info/dpg/ajax/companyProductReview.ajax.php",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+                "X-Requested-With": "XMLHttpRequest",
+                "Cookie": cookie_header
+            }
 
-            response = await self.page.request.get(url)
+            response = await self.page.request.get(url, headers=headers, timeout=10000)
             html = await response.text()
             soup = BeautifulSoup(html, "html.parser")
 
--- src/service/reveiw_batch_collector.py
+++ src/service/reveiw_batch_collector.py
@@ -4,6 +4,11 @@ from playwright.async_api import async_playwright
 from src.parser.asynchronous.review_parser import DanawaReviewAsyncParser
 from src.storage.file_storage import save_as_json
 
+async def limited_collect_review(semaphore, *args, **kwargs):
+    async with semaphore:
+        return await collect_review(*args, **kwargs)
+
+
 async def collect_review(page, product_no: str, sub_category: str, base_dir: str) -> dict:
     parser = DanawaReviewAsyncParser(page)
     reviews = await parser.get_reviews(product_no)
@@ -20,6 +25,7 @@ async def collect_review(page, product_no: str, sub_category: str, base_dir: str
 
 async def collect_reviews_concurrently(product_list: List[dict], sub_category: str, base_dir: str) -> Dict[str, List[str]]:
     result = {}
+    semaphore = asyncio.Semaphore(10)
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         context = await browser.new_context()
@@ -28,7 +34,10 @@ async def collect_reviews_concurrently(product_list: List[dict], sub_category: s
         for prod in product_list:
             page = await context.new_page()
             product_no = prod.id.replace("productItem", "")
-            tasks.append(collect_review(page, product_no, sub_category, base_dir))
+            tasks.append(
+                limited_collect_review(semaphore, page, product_no, sub_category, base_dir)
+            )
+
 
         results = await asyncio.gather(*tasks)
         await browser.close()
